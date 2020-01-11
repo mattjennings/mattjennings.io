370b608df35a2d88084a74b6997baf91b6de4f68
matt jennings <mattjennings44@gmail.com>
2020-01-11 13:01:55 -0700

    fix 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,23 +1,22 @@
-import React from "react"
+import React from 'react'
 
-import Layout from "../components/Layout"
-import SEO from "../components/SEO"
-import { Typography } from "@material-ui/core"
+import SEO from '../components/SEO'
+import { Typography } from '@material-ui/core'
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <SEO title="404: Not found" />
-    <Typography variant="h5" align="center" style={{ padding: "30px 0" }}>
+    <Typography variant="h5" align="center" style={{ padding: '30px 0' }}>
       oops, this page doesn't exist
     </Typography>
 
-    <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+    <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
       <img
         src="https://media.giphy.com/media/1rK7WULnc8YgdmVIBz/giphy.gif"
         alt="philadelphia flyers mascot falling down"
       />
     </div>
-  </Layout>
+  </>
 )
 
 export default NotFoundPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
-import { Typography, Grid } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 import React from 'react'
-import Layout from '../components/Layout'
 import PortfolioCard from '../components/PortfolioCard'
 import SEO from '../components/SEO'
 import { motion } from 'framer-motion'
