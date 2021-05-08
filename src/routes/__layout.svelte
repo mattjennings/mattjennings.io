184dc35d136cb247b3906c0b0cacba2c9935194a
Matt Jennings <mattjennings44@gmail.com>
2021-05-07 20:37:13 -0500

    remove max-width from prose

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -13,7 +13,9 @@
       <NavLink href="/blog">blog</NavLink>
     </nav>
 
-    <main class="flex flex-col flex-grow prose dark:prose-dark prose-lg sm:prose-xl py-4 px-4">
+    <main
+      class="flex flex-col w-full flex-grow prose dark:prose-dark prose-lg sm:prose-xl py-4 px-4"
+    >
       <slot />
     </main>
   </div>
