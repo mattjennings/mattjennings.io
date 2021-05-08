e8fce1c4efa253d5e2cc21bf9becde8eabc7b333
Matt Jennings <mattjennings44@gmail.com>
2021-05-07 22:30:55 -0500

    use custom domain for fathom

--- src/lib/components/Fathom.svelte
+++ src/lib/components/Fathom.svelte
@@ -1,12 +1,12 @@
 <script>
   import { browser } from '$app/env'
-
   import { page } from '$app/stores'
   import * as Fathom from 'fathom-client'
   import { onMount } from 'svelte'
 
-  onMount(() => {
+  onMount(async () => {
     Fathom.load('IDGMCFHT', {
+      url: 'https://wasp.mattjennings.io/script.js',
       includedDomains: ['mattjennings.io']
     })
   })
