44631612d7fa460cf82a31033dd584a29b197f63
Matt Jennings <me@mattjennings.io>
2022-01-09 01:48:14 +0000

    config gitpod

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,10 @@ import mdsvexConfig from './mdsvex.config.js'
 import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-static'
 
+const GITPOD_URL = process.env.GITPOD_URL 
+
+console.log(GITPOD_URL)
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', ...mdsvexConfig.extensions],
@@ -27,7 +31,16 @@ const config = {
       assets: 'public'
     }),
     vite: {
+
       server: {
+        hmr: GITPOD_URL ? 
+        {
+          clientPort: 443,
+          port: 443,
+          protocol: 'wss',
+          host: GITPOD_URL.replace('https://', '')
+        } 
+        : true,
         fs: {
           allow: ['./']
         }
