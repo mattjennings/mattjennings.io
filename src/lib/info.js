50732068dfbcc3c6fb5d405ea8682e83e81fbebe
Matt Jennings <me@mattjennings.io>
2022-10-13 12:49:32 -0500

    fix appetize link

--- src/lib/info.js
+++ src/lib/info.js
@@ -7,7 +7,7 @@ export const name = `${firstName} ${lastName}`
 // Used for the landing page & footer of posts
 export const avatar = 'https://api.lorem.space/image/face?w=250&h=250'
 export const bio = `\
-I'm ${firstName}, a Software Engineer based in Canada working for <a href="https://appetize">Appetize.</a>
+I'm ${firstName}, a Software Engineer based in Canada working for <a href="https://appetize.io">Appetize.</a>
 `
 export const bioText = bio.replace(/<[^>]*>/g, '')
 
