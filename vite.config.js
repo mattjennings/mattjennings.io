0a7983d6bd50f9a3a42763ad634869cf33a12c18
Matt Jennings <me@mattjennings.io>
2022-10-13 19:37:43 -0500

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import satori from 'satori-jsx/vite'
 import fs from 'fs'
 
 export default defineConfig({
-  plugins: [sveltekit(), satori(), base64(['.ttf', '.otf'])],
+  plugins: [sveltekit(), satori(), base64Fonts(['.ttf', '.otf'])],
   // allows vite access to ./posts
   server: {
     fs: {
@@ -13,9 +13,9 @@ export default defineConfig({
   }
 })
 
-function base64(ext) {
+function base64Fonts(ext) {
   return {
-    name: 'vite-plugin-array-buffer',
+    name: 'vite-plugin-base64-fonts',
     resolveId(id) {
       return ext.some((e) => id.endsWith(e)) ? id : null
     },
