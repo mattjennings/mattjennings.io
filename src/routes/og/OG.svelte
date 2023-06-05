e99a9d2fa63484bb4b79ba3cb648da25499479cc
Matt Jennings <me@mattjennings.io>
2023-06-04 19:02:51 -0500

    fix zindex error

--- src/routes/og/OG.svelte
+++ src/routes/og/OG.svelte
@@ -5,17 +5,17 @@
 </script>
 
 <div tw="bg-white flex flex-col w-full h-full items-center justify-center" style:font-size="80px">
-  <div tw="flex absolute -z-10" style:top="-100%" style:left="-55%">
+  <div tw="flex absolute" style:top="-100%" style:left="-55%">
     <Splatter fill="#35B3A2" scale={3} />
   </div>
-  <div tw="flex absolute -z-10" style:top="50%" style:left="25%">
+  <div tw="flex absolute" style:top="50%" style:left="25%">
     <Splatter fill="#35B3A2" scale={2} />
   </div>
 
-  <div tw="flex absolute -z-10" style:top="-280px" style:left="1050px">
+  <div tw="flex absolute" style:top="-280px" style:left="1050px">
     <Splatter fill="#F23B8F" scale={3} />
   </div>
-  <div tw="flex absolute -z-10" style:top="-920px" style:left="500px">
+  <div tw="flex absolute" style:top="-920px" style:left="500px">
     <Splatter fill="#F23B8F" scale={2} />
   </div>
   <div
